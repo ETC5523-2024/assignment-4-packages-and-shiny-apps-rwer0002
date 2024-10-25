7d49eef8394aa6a9a664d554e04ad5d7876bcb06
rwer0002 <rwer0002@student.monash.edu>
2024-10-25 23:48:38 +1100

    Adding roxygen skeleton for function

--- R/launch_app.R
+++ R/launch_app.R
@@ -1,4 +1,10 @@
+
+#' Launch Shiny app
+#'
+#' @return
 #' @export
+#'
+#' @examples
 launch_app <- function() {
   app_dir <- system.file("grasslandbirds-app", package = "grasslandbirds")
   shiny::runApp(app_dir, display.mode = "normal")
